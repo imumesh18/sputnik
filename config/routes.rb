c3c5f2d59bf5d47af036e7f55907013846c01ddc
Admin <admin@charlie01.local>
2018-07-24 17:42:49 +0530

    Basic wireframe for login and signup created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get '/sign_up' => 'sign_up#index'
+  get '/login' => 'login#index'
 end
