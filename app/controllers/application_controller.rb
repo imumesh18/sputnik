cf24a4def0a869618fb0f2e14109706dfd325de6
Umesh Yadav <umesh.y.aux@go-jek.com>
2018-07-27 12:41:12 +0530

    Added sentry error monitoring and logging.
    Signed-off-by: Umesh Yadav <umesh.y.aux@go-jek.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-
+    before_action :set_raven_context
     private
     
+    def set_raven_context
+      Raven.user_context(id: session[:current_user_id])
+      Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+    end
+
     def logged_in?
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
