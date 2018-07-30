044ab0b949e69c910a2952f7478e0fd49d528876
Admin <admin@charlie01.local>
2018-07-30 21:53:43 +0530

    root route replaced at top for a better convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  root 'users#home'
   get 'signup', to: 'users#signup', as: 'signup'
   get 'login', to: 'sessions#login', as: 'login'
   get 'logout', to: 'sessions#logout', as: 'logout'
   get 'profile', to: 'sessions#profile', as: 'profile'
   get 'users/verify/:token', to: 'users#verify'
-  resources :users
-  resources :sessions
+  resources :users, :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#home'
 end
